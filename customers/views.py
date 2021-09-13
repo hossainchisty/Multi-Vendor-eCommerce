de99deb88f3955e59743d5629ce7ff52957e049b
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-09-13 19:34:49 +0600

    features✨ customer profile and edit options🚀

--- customers/views.py
+++ customers/views.py
@@ -1,14 +1,36 @@
 from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
 from django.views.generic import CreateView
-
+from django.contrib.auth.decorators import login_required
+from .forms import CustomerSignUpForm, CustomerUpdateForm
 from .models import User
 
-from .forms import CustomerSignUpForm
-from .models import Customer
+
+@login_required(login_url='customer_sign_in')
+def CustomerProfile(request):
+    context = {
+        'customer': request.user.customer,
+    }
+    return render(request, 'customer/customer_profile.html', context)
+
+
+@login_required(login_url='customer_sign_in')
+def CustomerProfileUpdate(request):
+    if request.method == 'POST':
+        form = CustomerUpdateForm(request.POST, instance=request.user.customer)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('customer_profile')
+    else:
+        form = CustomerUpdateForm(instance=request.user.customer)
+        context = {
+            'form': form,
+        }
+        return render(request, 'customer/customer_profile_edit.html', context)
 
 
 class CustomerSignUpView(CreateView):

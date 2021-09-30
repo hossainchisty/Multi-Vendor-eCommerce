91f0b4fea77767ef38518daf22360f6723661852
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-09-30 13:42:39 +0600

    Add autocomplete feature✨

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('search', views.search_api, name='search'),
+    path('product', views.search, name='search'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+from django.db.models import Q
+from django.http import Http404, JsonResponse
 from django.shortcuts import render
 from product.models import Product
 
@@ -5,6 +7,41 @@ from product.models import Product
 def home(request):
     products = Product.objects.all()
     return render(request, 'core/home.html', {'products': products})
+
+
+def search_api(request):
+    ''' Search API for autocomplete '''
+    queryset = request.GET.get('query')
+    if queryset:
+        products = Product.objects.filter(Q(title__icontains=queryset) | Q(
+            slug__icontains=queryset) | Q(description__icontains=queryset))
+        jsonPayload = []
+        for product in products:
+            jsonPayload.append({
+                'title': product.title,
+                'slug': product.slug,
+                # 'image': print(type(product.image)),
+                'url': product.url,
+                'description': product.description,
+            })
+
+        return JsonResponse({
+            'status': 200,
+            'data': jsonPayload
+        })
+    else:
+        raise Http404
+
+
+def search(request):
+    ''' redirect search result  '''
+    queryset = request.GET.get('query')
+    if queryset:
+        products = Product.objects.filter(Q(title__icontains=queryset) | Q(
+            slug__icontains=queryset) | Q(description__icontains=queryset))
+        return render(request, 'core/search.html', {'results': products})
+    else:
+        raise Http404
 
 
 def about(request):

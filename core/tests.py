d9a26a24fa5cb4620037e1e7a9af35641b023669
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-10-24 00:00:08 +0600

    test: unit test case  for core urls ✨✔

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomeTestsUrls(TestCase):
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_home_url(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_about_url(self):
+        url = reverse('about')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_contact_url(self):
+        url = reverse('contact')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)

853812610b6735539bd9f0affb0cba75c2971f99
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-11-03 20:52:24 +0600

    fix: customer profile image✨

--- customers/models.py
+++ customers/models.py
@@ -1,7 +1,9 @@
+from cloudinary.models import CloudinaryField
+from django_countries.fields import CountryField
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
-from django_countries.fields import CountryField
 from model.common_fields import BaseModel
 
 from .manager import UserManager
@@ -27,6 +29,7 @@ class Customer(BaseModel):
     user = models.OneToOneField(
         User, related_name='customer', on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255)
+    image = CloudinaryField('avatar', null=True, blank=True)
     email = models.EmailField()
     phone_regex = RegexValidator(
         regex=r'^\+?880?\d{9,11}$', message="Phone number must be entered in the format: '+8801234233566'. Up to 11 digits allowed.")
@@ -38,6 +41,7 @@ class Customer(BaseModel):
     def __str__(self):
         return self.full_name
 
+    @property
     def get_user_full_name(self):
         """Returns the first_name plus the last_name, with a space in between."""
         full_name = f"{self.full_name}"

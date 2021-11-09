a8ce19741001b318810e40d0c3296bd58ba66517
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-11-09 14:17:02 +0600

    test: customer urls testing✔

--- customers/test/test_customer_url.py
+++ customers/test/test_customer_url.py
@@ -1,7 +1,7 @@
-from django.urls import reverse
-from django.test import TestCase, override_settings
 from customers import views
-# from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
+from django.test import TestCase, override_settings
+from django.urls import reverse
 
 
 class TestCustomerUrl(TestCase):
@@ -10,10 +10,10 @@ class TestCustomerUrl(TestCase):
         response = self.client.get(reverse('customer_profile'))
         self.assertRedirects(response, reverse('customer_sign_in') + '?next=/customer/profile/')
 
-    # @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
-    # def test_anonymous_cannot_see_change_password_page(self):
-    #     response = self.client.get(reverse('change_password'))
-    #     self.assertRedirects(response, reverse('customer_sign_in') + '?next=/customer/change/password/')
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_anonymous_cannot_see_change_password_page(self):
+        response = self.client.get(reverse('change_password'))
+        self.assertRedirects(response, reverse('customer_sign_in') + '?next=/customer/change/password/')
 
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_anonymous_cannot_see_profile_update_url(self):
@@ -34,9 +34,9 @@ class TestCustomerUrl(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.resolver_match.func.__name__, views.CustomerSignUpView.__name__)
 
-    # @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
-    # def test_customer_sign_out_view(self):
-    #     ''' test the sign out view '''
-    #     response = self.client.get(reverse('customer_sign_out'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertEqual(response.resolver_match.func.__name__, auth_views.LogoutView.as_view().__name__)
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_customer_sign_out_url(self):
+        ''' test the sign out view '''
+        response = self.client.get(reverse('customer_sign_out'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.resolver_match.func.__name__, auth_views.LogoutView.as_view().__name__)

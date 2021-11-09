6cecc6d4210068d62cfe548c9dc56a832db2baf8
Hossain Chisty <62835101+hossainchisty@users.noreply.github.com>
2021-11-09 16:40:23 +0600

    fix: paid date and delivered date✔

--- order/models.py
+++ order/models.py
@@ -17,10 +17,10 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=6, decimal_places=2)
 
     isPaid = models.BooleanField(default=False)
-    paid_date = models.DateTimeField(auto_now_add=True)
+    paid_date = models.DateTimeField()
 
     isDelivered = models.BooleanField(default=False)
-    delivered_date = models.DateTimeField(auto_now_add=True)
+    delivered_date = models.DateTimeField()
 
     isCancelled = models.BooleanField(default=False)
     isReturn = models.BooleanField(default=False)
@@ -49,6 +49,44 @@ class OrderItem(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    @property
+    def get_total_item_price(self):
+        '''
+        This method is used to get the total price of the item.
+        '''
+        return self.quantity * self.product.price
+
+    @property
+    def get_total_item_discount(self):
+        '''
+        This method is used to get the total discount of the item.
+        '''
+        return self.quantity * self.product.discount
+
+    @property
+    def get_amount_saved(self):
+        '''
+        This method is used to get the amount saved of the item.
+        '''
+        return self.get_total_item_price() - self.get_total_item_discount()
+
+    @property
+    def get_final_price(self):
+        '''
+        This method is used to get the final price of the item.
+        '''
+        if self.product.discount:
+            return self.get_total_item_discount()
+        return self.get_total_item_price()
+
+    @property
+    def get_delivery_date(self):
+        return self.order.delivered_date
+
+    @property
+    def get_delivery_status(self):
+        return self.order.status
 
     def __str__(self):
         return self.product.title
